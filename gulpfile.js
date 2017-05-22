50367c8cd175de5047f7935c79ea5ee814dec1de
Ben Metzger and Brian Fan <ben+brian@>
2017-05-22 14:44:51 -0700

    add openweathermap API

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,7 @@ gulp.task('serve', function() {
 
   gulp.watch(['js/*.js'], ['jsBuild']);
   gulp.watch(['bower.json'], ['bowerBuild']);
-
+  gulp.watch(['*.html'], ['htmlBuild']);
 });
 
 gulp.task('bowerBuild', ['bower'], function(){
@@ -38,6 +38,10 @@ gulp.task('bowerBuild', ['bower'], function(){
 });
 
 gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
+  browserSync.reload();
+});
+
+gulp.task('htmlBuild', function() {
   browserSync.reload();
 });
 
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -22,3 +22,16 @@ $(document).ready(function(){
 $(document).ready(function(){
   $('#time').text(moment());
 });
+
+var apiKey = "e4e7cba7a8388ccfd7386a79f5d0536e";
+
+$(document).ready(function() {
+  $('#weather-location').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $('.showWeather').text("The city you have chosen is " + city + ".");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      $('.showWeather').text("The humidity in " + city + " is " + (response['weather'][0]['description']) + "%");
+    });
+  });
+});
